54d60105fcb6560f5d9aeedb7317fdf30040d815
pionell <skypeon@gmail.com>
2017-03-22 15:45:02 +0200

    pridėtas registracijos langas, pridėjau gradle scriptus (paleidimui)

--- app/src/main/java/tk/folrikas/bitinelis/LoginActivity.java
+++ app/src/main/java/tk/folrikas/bitinelis/LoginActivity.java
@@ -1,7 +1,10 @@
 package tk.folrikas.bitinelis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button mygtukas = (Button) findViewById(R.id.prisijungti);
+        Button mygtukas2 = (Button) findViewById(R.id.registruotis);
+        mygtukas.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent activityChangeIntent = new Intent(LoginActivity.this, MenuActivity.class);
+
+                // currentContext.startActivity(activityChangeIntent);
+
+                LoginActivity.this.startActivity(activityChangeIntent);
+            }
+        });
+        mygtukas2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent activityChangeIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+
+                // currentContext.startActivity(activityChangeIntent);
+
+                LoginActivity.this.startActivity(activityChangeIntent);
+            }
+        });
     }
 }
