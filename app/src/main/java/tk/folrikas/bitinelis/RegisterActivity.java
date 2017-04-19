9f52bee17457f9dc0365537f0f1a68a207455941
Tidish <Tidish@inbox.lt>
2017-04-19 14:41:51 +0300

    padaryta registracija, įsiterpia naujas vartotojas į esamą sistemą (duomenų bazę)

--- app/src/main/java/tk/folrikas/bitinelis/RegisterActivity.java
+++ app/src/main/java/tk/folrikas/bitinelis/RegisterActivity.java
@@ -7,12 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RegisterActivity extends Activity {
 
+    private static final String REGISTER_URL = "Adresiukas";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,13 +72,14 @@ public class RegisterActivity extends Activity {
                     focusView.requestFocus();
                 } else {
                     Register REG = new Register(username, password, email);
-                        Toast.makeText(RegisterActivity.this,
+                        /*Toast.makeText(RegisterActivity.this,
+
                                 "objekte: " +
                                 "\nVartotojo vardas: " + REG.getUsername()+
                                 "\nSlaptazodis: " + REG.getPassword()+
                                 "\npasto adresas: " + REG.getEmail(),
-                                Toast.LENGTH_SHORT).show();
-
+                                Toast.LENGTH_SHORT).show();*/
+                    registerUser(REG.getUsername(),REG.getPassword(),REG.getEmail());
                     Intent intent = new Intent(RegisterActivity.this, MenuActivity.class);
                     startActivity(intent);
                 }
@@ -100,6 +107,54 @@ public class RegisterActivity extends Activity {
 
         Matcher matcher = pattern.matcher(credentials);
         return matcher.matches();
+    }
+    private void registerUser(String username, String password, String email) {
+        String urlSuffix = "?username="+username+"&password="+password+"&email="+email;
+        class RegisterUser extends AsyncTask<String, Void, String> {
+
+            ProgressDialog loading;
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(RegisterActivity.this, "Prašome palaukti",null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String s = params[0];
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(REGISTER_URL+s);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    // byethost naudoja antibot sistema, todel reikia kiekvienam rankutėmis suvesti cookie turinį,
+                    // kuris pas kiekviena bus skirtingas. kaip tai padaryti zemiau nuoroda
+                    // http://stackoverflow.com/questions/31912000/byethost-server-passing-html-values-checking-your-browser-with-json-string
+                    con.setRequestProperty("Cookie", "__test=7a4d917e220fbf9a55cab3046bd1a3b7; expires=2038 m. sausio 1 d., penktadienis 01:55:55; path=/");
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String result;
+
+                    result = bufferedReader.readLine();
+
+                    return result;
+                }catch(Exception e){
+                    return null;
+                }
+            }
+        }
+
+        RegisterUser ru = new RegisterUser();
+        ru.execute(urlSuffix);
     }
 
 }
